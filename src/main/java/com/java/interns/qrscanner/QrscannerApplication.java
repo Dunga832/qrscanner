cac0d0423b6f4fc6c64d51094b558d97b803d2c8
Clayton <clayton27121996@gmail.com>
2023-03-22 17:29:45 +0200

    Initial Template, Including Lombok.

---
+++ src/main/java/com/java/interns/qrscanner/QrscannerApplication.java
@@ -0,0 +1,13 @@
+package com.java.interns.qrscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrscannerApplication.class, args);
+	}
+
+}
